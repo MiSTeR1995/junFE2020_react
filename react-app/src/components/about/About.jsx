38ad5e4d2410079f34ed60e50c41dac614965cf8
Alex <36799721+MiSTeR1995@users.noreply.github.com>
2020-04-27 19:52:55 +0300

    123

--- react-app/src/components/about/About.jsx
+++ react-app/src/components/about/About.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import axios from "axios";
 
+export default class About extends React.Component {
+    state = {
+        persons: [],
+        isLoading: true
+    };
 
-function About () {
-    return (
-        <div>Some words about us</div>
-    );
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+            setTimeout( _ => this.setState({ persons: res.data, isLoading: false }), 1000);
+        });
+    }
+    onDeleteClick(id) {
+        // axios.delete()
+    }
+    render() {
+
+        return (
+            <div>
+                {this.state.isLoading ? (
+                    <div>Подождите, мы собираем данные.</div>
+                ) : (
+                    this.state.persons.map((person) => (
+                        <div id="user-{person.id}" className="user-item">
+                            <div className="item-actions">
+                                <button onclick="onDeleteClick({person.id})">
+                                    Delete
+                                </button>
+                            </div>
+                            <div className="item-left">
+                                <div className="user-name">{person.name}</div>
+                                <div className="user-mail">{person.email}</div>
+                            </div>
+                            <div className="item-right">
+                                <div className="user-phone">{person.phone}</div>
+                                <div className="user-company">
+                                    <div className="company-name">
+                                        {person.company.name}
+                                    </div>
+                                    <div className="company-sphere">
+                                        {person.company.catchPhrase}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                )}
+            </div>
+        );
+    }
 }
-export default About;
+// export default About;
